c7f1dc9e3f9fe37166ed1943b10c0d2c62d24a12
diazdjul19 <diazdjul19@gmail.com>
2021-08-15 15:47:57 +0700

    commit-1.1

--- database/migrations/2019_12_26_114331_add_role_to_users.php
+++ database/migrations/2019_12_26_114331_add_role_to_users.php
@@ -25,8 +25,6 @@ class AddRoleToUsers extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->after('id');
-        });
+        
     }
 }
