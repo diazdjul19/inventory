38f35c4ae7ed7da03e9e402cb8e08498ffba3c65
Diaz Djuliansyah <52434835+diazdjul19@users.noreply.github.com>
2021-08-18 14:22:53 +0700

    Update snappy.php

--- config/snappy.php
+++ config/snappy.php
@@ -56,14 +56,14 @@ return [
 
     'pdf' => array(
         'enabled' => true,
-        'binary' => base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf'),
+        'binary' => '/app/bin/wkhtmltopdf',
         'timeout' => false,
         'options' => array(),
         'env' => array(),
     ),
     'image' => array(
         'enabled' => true,
-        'binary' => 'vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltoimage',
+        'binary' => '/app/bin/wkhtmltoimage',
         'timeout' => false,
         'options' => array(),
         'env' => array(),
